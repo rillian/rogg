cc0b919d93ed7ed0622a8688ef92bd083c547f28
Ralph Giles <giles@xiph.org>
2005-06-08 22:00:35 +0000

    Quick and dirty library for in-memory Ogg page parsing.
    We can parse page headers, and update crc's after changes
    (that part is untested).
    
    Includes an example using mmap to dump page data.
    
    
    git-svn-id: https://svn.xiph.org/experimental/giles/rogg@9404 0101bb08-14d6-0310-b084-bc0e0c8e3800

---
+++ rogg.h
@@ -0,0 +1,83 @@
+/*
+   Copyright (C) 2005 Xiph.org Foundation
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE ORGANISATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* simple library for dealing with Ogg pages in memory */
+
+#ifndef _ROGG_H_
+#define _ROGG_H_
+
+#include <stdint.h>
+
+/* parsed header struct */
+typedef struct _rogg_page_header rogg_page_header;
+struct _rogg_page_header {
+  /* literal header fields, translated for byte order */
+  unsigned char *capture;	/* 4 bytes */
+  unsigned char version;	/* 1 byte */
+  unsigned char flags;		/* 1 byte */
+  uint64_t granulepos;		/* 8 bytes */
+  uint32_t serialno;		/* 4 bytes */
+  uint32_t sequenceno;		/* 4 bytes */
+  uint32_t crc;			/* 4 bytes */
+  unsigned char segments;	/* 1 byte */
+  unsigned char *lacing;	/* segments bytes */
+  unsigned char *data;		/* start of page data */
+  /* derived flags */
+  int continued;
+  int bos, eos;
+  int length;
+};
+
+/* Ogg header entry offsets */
+#define ROGG_OFFSET_CAPTURE 0
+#define ROGG_OFFSET_VERSION 4
+#define ROGG_OFFSET_FLAGS 5
+#define ROGG_OFFSET_GRANULEPOS 6
+#define ROGG_OFFSET_SERIALNO 14
+#define ROGG_OFFSET_SEQUENCENO 18
+#define ROGG_OFFSET_CRC 22
+#define ROGG_OFFSET_SEGMENTS 26
+#define ROGG_OFFSET_LACING 27
+
+/* little endian i/o */
+void rogg_write_uint64(unsigned char *p, uint64_t v);
+void rogg_write_uint32(unsigned char *p, uint32_t v);
+void rogg_write_uint16(unsigned char *p, uint16_t v);
+void rogg_read_uint64(unsigned char *p, uint64_t *v);
+void rogg_read_uint32(unsigned char *p, uint32_t *v);
+void rogg_read_uint16(unsigned char *p, uint16_t *v);
+
+/* calculate the length of the page starting at p */
+void rogg_get_length(unsigned char *p, int *length);
+
+/* parse out the header fields of the page starting at p */
+void rogg_parse_header(unsigned char *p, rogg_page_header *header);
+
+/* recompute and store a new crc on the page starting at p */
+void rogg_update_crc(unsigned char *p);
+
+#endif /* _ROGG_H */
---
+++ rogg_pagedump.c
@@ -0,0 +1,90 @@
+/*
+   Copyright (C) 2005 Xiph.org Foundation
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions
+   are met:
+
+   - Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+   ``AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+   PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE ORGANISATION OR
+   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+/* simple script example for the rogg library */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <rogg.h>
+
+void print_header_info(FILE *out, rogg_page_header *header)
+{
+  fprintf(out, " Ogg page serial %08x seq %d (%5d bytes)",
+	header->serialno, header->sequenceno, header->length);
+  fprintf(out, (header->continued) ? " c" : "  ");
+  fprintf(out, " %lld", header->granulepos);
+  fprintf(out, (header->bos) ? " bos" : "");
+  fprintf(out, (header->eos) ? " eos" : "");
+  fprintf(out, "\n");
+}
+
+int main(int argc, char *argv[])
+{
+  int f, i;
+  unsigned char *p, *q, *e;
+  struct stat s;
+  rogg_page_header header;
+
+  for (i = 1; i < argc; i++) {
+    f = open(argv[i], O_RDWR);
+    if (f < 0) {
+	fprintf(stderr, "couldn't open '%s'\n", argv[i]);
+	continue;
+    }
+    if (fstat(f, &s) < 0) {
+	fprintf(stderr, "couldn't stat '%s'\n", argv[i]);
+	close(f);
+	continue;
+    }
+    p = mmap(0, s.st_size, PROT_READ|PROT_WRITE,
+	MAP_SHARED, f, 0);
+    if (p == NULL) {
+	fprintf(stderr, "could mmap '%s'\n", argv[i]);
+	close(f);
+	continue;
+    }
+    fprintf(stdout, "Dumping Ogg file '%s'\n", argv[i]);
+    e = p + s.st_size;
+    q = p;
+    while (q < e) {
+	rogg_parse_header(q, &header);
+	print_header_info(stdout, &header);
+	q += header.length;
+    }
+    munmap(p, s.st_size);
+    close(f);
+  }
+  return 0;
+}
